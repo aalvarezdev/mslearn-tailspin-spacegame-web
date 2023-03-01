ab06d76b0c40a104b728cb820b1a8d7b7ffa459a
Alvaro Alvarez <alvarezdev@outlook.com>
2023-03-01 12:10:22 -0800

    Uploading test project

---
+++ Tailspin.SpaceGame.Web.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tailspin.SpaceGame.Web.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
